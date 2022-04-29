b284cc3a618d44b88b0aea195082d75d811f9d5f
Esdras Oliveira <esdrasjesus5@gmail.com>
2022-04-29 14:24:55 -0300

    build: add npm dependency ts-node-dev

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,17 @@
 import express from 'express';
+import Controller from './controllers/controller';
 
 require('express-async-errors');
 
 class App {
   public app: express.Application;
 
+  public controller = new Controller();
+
   constructor() {
     this.app = express();
     this.app.use(express.json());
+    this.app.post( '/form', this.controller.create)
   }
 
   public startServer(PORT: string | number = 3001): void {
