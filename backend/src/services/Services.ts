88acf8fa1954e572be0023a3d43d2b139a8deaf2
Esdras Oliveira <esdrasjesus5@gmail.com>
2022-04-29 15:45:02 -0300

    build: installed eslint

--- backend/src/services/Services.ts
+++ backend/src/services/Services.ts
@@ -2,11 +2,18 @@ import { ResponseError, ResponseFormResult } from "../interface/ResponsesInterfa
 import { ServiceInterface } from "../interface/ServiceInterface";
 import Model from "../models/Model";
 import { FormType } from "../types/FormType";
+import Validation from "../validations/Validation";
 
 class Service implements ServiceInterface {
+
+  private validation = new Validation();
+
   constructor(public model = new Model()){}
 
   create = async (obj: FormType): Promise<ResponseError | ResponseFormResult> => {
+    if (!this.validation.form(obj)) {
+      return { status: 400, response: { error:  'Bad Request' }};
+    }
     const response = await this.model.create(obj);
     if (response === null) {
       return { status: 500, response: { error: 'Internal Server Error'}}
