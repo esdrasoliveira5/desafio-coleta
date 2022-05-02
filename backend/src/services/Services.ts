859df6e7ffb7c19d9deccb5669714a1cb9ad5766
Esdras Oliveira <esdrasjesus5@gmail.com>
2022-05-02 10:03:43 -0300

    refactor: validation and Service create fixed

--- backend/src/services/Services.ts
+++ backend/src/services/Services.ts
@@ -1,3 +1,4 @@
+import FormResult from '../helpers/FormResult';
 import {
   ResponseError,
   ResponseFormResult,
@@ -10,15 +11,20 @@ import Validation from '../validations/Validation';
 class Service implements ServiceInterface {
   private validation = new Validation();
 
+  private formResult = new FormResult();
+
   constructor(public model = new Model()) { }
 
   create = async (obj: FormType):
   Promise<ResponseError | ResponseFormResult> => {
-    if (!this.validation.form(obj)) {
-      return { status: 400, response: { error: 'Bad Request' } };
+    const valid = this.validation.form(obj);
+    if (valid !== true) {
+      return { status: 400, response: valid };
     }
+    
+    const result = this.formResult.result(obj);
 
-    const response = await this.model.create(obj);
+    const response = await this.model.create({ ...obj, ...result });
     if (response === null) {
       return { status: 500, response: { error: 'Internal Server Error' } };
     }
--- backend/src/validations/Validation.ts
+++ backend/src/validations/Validation.ts
@@ -8,11 +8,10 @@ class Validation {
   };
 
   nowOrMaybe = (resposta: string) => {
-    if (resposta === 'Sim' || resposta === 'Não') {
-      return true;
-    }
-    
-    if (resposta === 'Não Sei' || resposta === 'Agora') {
+    if (resposta === 'Sim' 
+    || resposta === 'Não' 
+    || resposta === 'Não Sei' 
+    || resposta === 'Agora!!') {
       return true;
     }
   };
@@ -29,10 +28,21 @@ class Validation {
 
   form = (data: FormType) => {
     const { pergunta1, pergunta2, pergunta3, pergunta4 } = data;
-    if (!this.yesOrNo(pergunta1)) return false;
-    if (!this.yesOrNo(pergunta2)) return false;
-    if (!this.nowOrMaybe(pergunta3)) return false;
-    if (!this.textBox(pergunta4)) return false;
+
+    if (!this.yesOrNo(pergunta1)) {
+      return { error: 'answer1 must be Sim or Não' };
+    }
+    if (!this.yesOrNo(pergunta2)) {
+      return { error: 'answer2 must be Sim or Não' };
+    }
+    if (!this.nowOrMaybe(pergunta3)) {
+      return { error: 'answer3 invalid' };
+    }
+    if (!this.textBox(pergunta4)) {
+      return { error: 'answer1 must be greater than 15 and less than 200' };
+    }
+
+    return true;
   };
 }
 
