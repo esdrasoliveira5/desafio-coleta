30c91bc23b145136c5256671cb1be14207c7c645
Esdras Oliveira <esdrasjesus5@gmail.com>
2022-05-02 11:39:46 -0300

    feat: created frontend react components Footer, Forms, Header

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Footer from './components/Footer';
+import Forms from './components/Forms';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Forms />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Forms.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Forms() {
+  return (
+    <form>
+      Forms
+    </form>
+  );
+}
+
+export default Forms;
