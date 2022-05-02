ba7c43a44db1a7cbfcb4b31986898822f4bf8b57
Esdras Oliveira <esdrasjesus5@gmail.com>
2022-05-02 13:13:54 -0300

    style: change index css style

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import { FooterStyled } from '../styles/Forms';
+import github from '../images/github.png';
+import linkedin from '../images/linkedin.png';
 
 function Footer() {
   return (
     <FooterStyled>
-      Create by Esdras Oliveira
+      <p>Created by Esdras Oliveira</p>
+      <div>
+        <a href="https://github.com/esdrasoliveira5" target="_blank" rel="noreferrer">
+          <img src={github} alt="github" />
+        </a>
+        <a href="https://www.linkedin.com/in/esdrasmoliveira/" target="_blank" rel="noreferrer">
+          <img src={linkedin} alt="linkedin" />
+        </a>
+      </div>
     </FooterStyled>
   );
 }
